4554ee1493b76d43948ca2cabc16d1ee134d31de
oazzat <oazzat19@gmail.com>
2018-11-11 11:12:44 -0500

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,22 @@
+class Course
+
+attr_accessor :title, :schedule, :description
+@@all = []
+
+def self.all
+  @@all
+end
+
+def self.reset_all
+  @@all = []
+end
+
+def initialize()
+  # @title = title
+  # @schedule = schedule
+  # @description = description
+  @@all << self
+end
 
 
+end
